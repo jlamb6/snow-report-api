5b2661fdbda8aadfc76fabe1b6382dae101c093f
Jake Lamb <jacobtaylorlamb@gmail.com>
2020-02-15 12:18:44 -0700

    add cors

--- api/snow-report.js
+++ api/snow-report.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 
 const SnowReports = require('../models/snow-report');
 const router = express.Router();
 
-router.get("/:resort", (req, res) => {
+router.get("/:resort", cors(), (req, res) => {
   SnowReports.find({resort: req.params.resort}).sort('-date').exec((err, report) => {
     if (err) {
       res.json({message: "an error occurred while finding snow reports for" + req.params.resort});
